4cc4e59ff1b86fcff045716ba5ca6e17ca2b54be
Dempsey1201 <1205342729@qq.com>
2019-05-30 21:19:23 +0800

    update student

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,4 @@
 <template>
-<<<<<<< HEAD
-<div class="d1">
-        <P>Design by:ape-men（唐佳成，邹文燕，章腾，陈仕炜，董明龙）</P>
-        <P>一体化考核项目</P>
-</div>
-=======
     <el-footer style="height: 120px;">
         <img id="footerlogo" src="../../src/assets/footerlogo.png">
         <div id="intro">
@@ -14,7 +8,6 @@
             <P>地址: https://github.com/ape-men</P>
         </div>
     </el-footer>
->>>>>>> 68dfe094f6193ec873827535c169cbd1b04335ee
 </template>
 
 <script>
@@ -25,18 +18,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-<<<<<<< HEAD
-.d1{
-    height: 90px;
-    border-top:1px solid rgba(0, 0, 0, 0.1);
-    background-color: rgba(49,169,204,0.5);
-    text-align: center;
-}
-.d1 p{
-    line-height: 20px;
-    color: white;
-    font-size:16px;
-=======
 .el-footer {
     border-top:2px solid rgba(0, 0, 0, 0.1);
     background-color: rgba(6, 137, 177, 0.808);
@@ -48,7 +29,6 @@ export default {
     line-height: 10px;
     color:white;
     font-size:14px;
->>>>>>> 68dfe094f6193ec873827535c169cbd1b04335ee
     font-weight: 500;
 }
 .el-footer #footerlogo,#intro {
