b19b143bd114bc5ad03eed93c80ff159dd341ce6
tenforests <434856668@qq.com>
2019-06-01 21:42:02 +0800

    logo footer header update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,7 @@
 <template>
-<el-row class="fa">
-        <el-col :span="20" :offset="2" class="d">
-            <P>Designed by:ape-men @一体化小组</P>
-            <p>唐佳成,章腾,陈仕炜,董明龙,邹文燕</p>
-            <P>小组地址: https://github.com/ape-men</P>
-        </el-col>
-</el-row>
+<div class="fa">
+           <a href="https://github.com/ape-men.html" target="_blank">Designed by:ape-men（唐佳成,章腾,陈仕炜,董明龙,邹文燕）@一体化小组</a>
+</div>
 </template>
 
 <script>
@@ -17,21 +13,16 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .fa{
-    min-height: 50px;
-    background-color: inherit;
+    border-top:1px solid rgba(0,0,0,0.1);
+     text-align: center;
+     padding-top: 10px;
 }
-.d {
-    border-top:1px solid rgba(0, 0, 0, 0.1);
-    background-color: #90d7ec;
-    text-align: center;
-    border-radius: 4px;
-    /* padding-bottom: 5px; */
-}
-.d p {
-    line-height: 10px;
-    color:white;
-    font-size:16px;
-    font-weight: 700;
+.fa a {
+    text-decoration: none;
+    line-height: 20px;
+    color:rgba(0,0,0,0.6);
+    font-size:15px;
+    font-weight: 450;
 }
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,22 @@
 <template>
-    <header>
+    <!-- <header>
         <div id="line1"></div>
         <div class="logo">
             <img id="headerlogo" src="../../src/assets/headerlogo.png">
             <div class="right">
                 <span style="padding:10px;">你好！</span>
-                <span><i class="el-icon-s-custom"> <a class="exit" ref="#">退出登录</a></i></span>
+                <span><i class="el-icon-s-custom">  <a class="exit" ref="#">退出登录</a></i></span>
             </div>
         </div>
         <div id="line2"></div>
-    </header>
+
+    </header> -->
+    <el-row class="c1">
+        <el-col :span="6" class ="head">
+            <img src="../../src/assets/new.png" width="60px" height="50px">
+            <span>在线考试系统</span>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
@@ -19,7 +26,28 @@ export default {
 </script>
 
 <style scoped>
-#line1 {
+.c1{
+    width: 85%;
+    position: fixed;
+    left: 0px;
+    right: 0px;
+        margin-left:auto;
+    margin-right:auto;
+    border-bottom: 1px solid rgba(0,0 ,0, 0.1);
+    padding-bottom:10px;
+}
+.head{
+    /* width: 600px; */
+    border-radius: 4px;
+    color:#31a9cc ;
+    font-weight: 900;
+    font-size: 30px;
+    line-height: 45px;
+}
+.head span,img{
+    vertical-align: middle;
+}
+/* #line1 {
     width: 100%;
     height: 10px;
     background-color: rgba(35, 107, 129, 0.808);
@@ -44,5 +72,5 @@ export default {
 .exit {
     text-decoration: none;
     cursor: pointer;
-}
+} */
 </style>
