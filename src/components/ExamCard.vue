57e59a6672ab450b4f2c2515f895e2139dee0fb6
tocque <364004564@qq.com>
2019-06-03 16:57:23 +0900

    restructuring /student

--- src/components/ExamCard.vue
+++ src/components/ExamCard.vue
@@ -1,157 +1,107 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column type="expand">
-      <template slot-scope="props">
-        <el-form label-position="left" inline  class="table">
-          <el-form-item label="考试名称">
-            <span>{{ props.row.考试名称 }}</span>
-          </el-form-item>
-          <el-form-item label="考试编码">
-            <span>{{ props.row.考试编码 }}</span>
-          </el-form-item>
-          <el-form-item label="截止时间">
-            <span>{{ props.row.截止时间 }}</span>
-          </el-form-item>
-          <el-form-item label="开始时间">
-            <span>{{ props.row.开始时间 }}</span>
-          </el-form-item>
-            <el-form-item label="考试科目">
-            <span>{{ props.row.考试科目 }}</span>
-          </el-form-item>
-          <el-form-item label="任课老师">
-            <span>{{ props.row.任课老师 }}</span>
-          </el-form-item>
-          <el-form-item label="考试限时">
-            <span>{{ props.row.考试限时 }}</span>
-          </el-form-item>
-            <el-form-item label="考试总分">
-            <span>{{ props.row.考试总分 }}</span>
-          </el-form-item>
-            <el-form-item label="及格分数">
-            <span>{{ props.row.及格分数 }}</span>
-          </el-form-item>
-          <el-form-item label="剩余可考次数">
-            <span>{{ props.row.剩余可考次数 }}</span>
-          </el-form-item>
-          <el-form-item label="是否立即可查成绩">
-            <span>{{ props.row.是否立即可查成绩 }}</span>
-          </el-form-item>
-        </el-form>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="考试编码"
-      prop="考试编码">
-    </el-table-column>
-    <el-table-column
-      label="考试名称"
-      prop="考试名称">
-    </el-table-column>
-    <el-table-column
-      label="截止时间"
-      prop="截止时间">
-    </el-table-column>
-  </el-table>
+    <li class="examCard">
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <a v-bind:href="info.examAddress">
+                    <span class="examName">【{{ info.courseName }}】{{ info.examName }}</span>
+                </a>
+                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <ul class="info">
+                <li>课程名称<span>{{ info.courseName }}</span></li>
+                <li>考试时间<span>{{ examTime }}</span></li>
+                <li>总分数<span>{{ info.totalScore}}分</span></li>
+            </ul>
+        </el-card>
+    </li>
 </template>
-
-<style>
-  .table {
-    font-size: 0;
-  }
-  .table label {
-    width: 180px;
-    color: #99a9bf;
-  }
-  .table .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 50%;
-}
-</style>
-
 
 <script>
 export default {
     name: 'ExamCard',
-    data() {
-        return {
-            tableData: [{
-                考试编码: '100000',
-                考试名称: '线性代数期末考',
-                截止时间: '2019-7-22',
-                开始时间: '2019-6-22',
-                考试科目: '线性代数',
-                任课老师: '张友',
-                考试限时: '90min',
-                考试总分: '100',
-                及格分数: '60',
-                剩余可考次数: '1',
-                是否立即可查成绩: '否'
-            }, {
-                考试编码: '100000',
-                考试名称: '线性代数期末考',
-                截止时间: '2019-7-22',
-                开始时间: '2019-6-22',
-                考试科目: '线性代数',
-                任课老师: '张友',
-                考试限时: '90min',
-                考试总分: '100',
-                及格分数: '60',
-                剩余可考次数: '1',
-                是否立即可查成绩: '否'
-            }, {
-                考试编码: '100000',
-                考试名称: '线性代数期末考',
-                截止时间: '2019-7-22',
-                开始时间: '2019-6-22',
-                考试科目: '线性代数',
-                任课老师: '张友',
-                考试限时: '90min',
-                考试总分: '100',
-                及格分数: '60',
-                剩余可考次数: '1',
-                是否立即可查成绩: '否'
-            }, {
-                考试编码: '100000',
-                考试名称: '线性代数期末考',
-                截止时间: '2019-7-22',
-                开始时间: '2019-6-22',
-                考试科目: '线性代数',
-                任课老师: '张友',
-                考试限时: '90min',
-                考试总分: '100',
-                及格分数: '60',
-                剩余可考次数: '1',
-                是否立即可查成绩: '否'
-            }]
+    props: {
+        info: {
+            examName: String,
+            courseName: String,
+            startTime: Date,
+            endTime: Date,
+            totalScore: Number,
+            examAddress: String,
+        }
+    },
+    computed: {
+        examTime: function () {
+            let from = this.info.startTime, to = this.info.endTime,
+                fromStr = "", toStr = "", diff = false;
+            for (let part of ["getFullYear", "getMonth", "getDay"]) {
+                fromStr += '/' + from[part]();
+                if (!diff && from[part]() != to[part]()) diff = true;
+                if (diff) toStr += '/' + to[part]();
+            }
+            fromStr += ' ' + from.getHours() + ':' + from.getMinutes().toString().padStart(2, '0');
+            toStr += ' ' + to.getHours() + ':' + to.getMinutes().toString().padStart(2, '0');
+            return fromStr.substring(1) + ' - ' + toStr.substring(1);
         }
     }
-    // props: {
-    //     info: {
-    //         examName: String,
-    //         courseName: String,
-    //         professor: String,
-    //         startTime: Date,
-    //         endTime: Date,
-    //         totalScore: Number,
-    //         examAddress: String,
-    //     }
-    // },
-    // computed: {
-    //     examTime: function () {
-    //         let from = this.info.startTime, to = this.info.endTime,
-    //             fromStr = "", toStr = "", diff = false;
-    //         for (let part of ["getFullYear", "getMonth", "getDay"]) {
-    //             fromStr += '/' + from[part]();
-    //             if (!diff && from[part]() != to[part]()) diff = true;
-    //             if (diff) toStr += '/' + to[part]();
-    //         }
-    //         fromStr += ' ' + from.getHours() + ':' + from.getMinutes().toString().padStart(2, '0');
-    //         toStr += ' ' + to.getHours() + ':' + to.getMinutes().toString().padStart(2, '0');
-    //         return fromStr.substring(1) + ' - ' + toStr.substring(1);
-    //     }
-    // }
 }
 </script>
+
+<style scoped>
+*{
+    margin: 0px;
+    padding: 0px;
+    outline: none;
+}
+li {
+    list-style: none;
+    vertical-align: bottom;
+}
+.examCard {
+    margin-top: 20px;
+    margin-bottom: 40px;
+    width: 650px;
+    height: 128px;
+}
+.undid {
+    width: 130px;
+    height: 120px;
+    position: relative;
+    display: inline-block;
+    font-size: 20px;
+}
+.exam {
+    width: 435px;
+    position: relative;
+    margin-left: 30px;
+    display: inline-block;
+}
+.examCard .examName a {
+    margin-top: 6px;
+    max-width: 275px;
+    font-size: 18px;
+    display: inline-block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color: #259fc8;
+    cursor: pointer;
+    text-decoration: none;
+}
+.examCard ul.info {
+    width: 435px;
+    height: 80px;
+    font-size: 14px;
+    color: #666;
+    font-family: Tahoma,Verdana,sans-serif;
+}
+.examCard ul.info li {
+    width: 407px;
+    height: 24px;
+    line-height: 20px;
+    display: inline-block;
+}
+.examCard ul.info li span {
+    margin-inline-start: 12px;
+    color: black;
+}
+</style>
--- src/components/NotifyCard.vue
+++ src/components/NotifyCard.vue
@@ -1,52 +1,12 @@
 <template>
-    <div class="not1">
-  <el-timeline>
-    <el-timeline-item timestamp="2019/6/12" placement="top">
-      <el-card>
-<h4 class="notiTitle">{{info.title}}</h4>
-        <p class="notiMain">{{ info.content }}</p>
-        <span class="notiTime">{{ notifyDate }} </span>
-        <span class="notiInfo">
-            来自：<span style="color: #259fc8;">{{ info.professor }}</span> 老师
-            <span style="color: #259fc8;"> 课程:【{{ info.courseName }}】</span>
-        </span>
-      </el-card>
-    </el-timeline-item>
-    <el-timeline-item timestamp="2019/6/3" placement="top">
-      <el-card>
-<h4 class="notiTitle">{{info.title}}</h4>
-        <p class="notiMain">{{ info.content }}</p>
-        <span class="notiTime">{{ notifyDate }} </span>
-        <span class="notiInfo">
-            来自：<span style="color: #259fc8;">{{ info.professor }}</span> 老师
-            <span style="color: #259fc8;"> 课程:【{{ info.courseName }}】</span>
-        </span>
-      </el-card>
-    </el-timeline-item>
-    <el-timeline-item timestamp="2019/6/1" placement="top">
-      <el-card>
-        <h4 class="notiTitle">{{info.title}}</h4>
-        <p class="notiMain">{{ info.content }}</p>
-        <span class="notiTime">{{ notifyDate }} </span>
-        <span class="notiInfo">
-            来自：<span style="color: #259fc8;">{{ info.professor }}</span> 老师
-            <span style="color: #259fc8;"> 课程:【{{ info.courseName }}】</span>
-        </span>
-      </el-card>
-    </el-timeline-item>
-    <el-timeline-item timestamp="2019/6/1" placement="top">
-      <el-card>
-        <h4 class="notiTitle">{{info.title}}</h4>
-        <p class="notiMain">{{ info.content }}</p>
-        <span class="notiTime">{{ notifyDate }} </span>
-        <span class="notiInfo">
-            来自：<span style="color: #259fc8;">{{ info.professor }}</span> 老师
-            <span style="color: #259fc8;"> 课程:【{{ info.courseName }}】</span>
-        </span>
-      </el-card>
-    </el-timeline-item>
-  </el-timeline>
-</div>
+<el-card>
+    <h4 class="notiTitle">{{ info.title }}</h4>
+    <p class="notiMain">{{ info.content }}</p>
+    <span class="notiInfo">
+        来自：<span style="color: #259fc8;">{{ info.professor }}</span> 老师
+        <span style="color: #259fc8;"> 课程:【{{ info.courseName }}】</span>
+    </span>
+</el-card>
 </template>
 
 <script>
@@ -58,52 +18,37 @@ export default {
             content: String,
             courseName: String,
             professor: String,
-            date: Date,
         }
     },
-    computed: {
-        notifyDate: function () {
-            let date = this.info.date;
-            return date.getFullYear() + '.' + date.getMonth() + '.' + date.getDay();
-        }
-    }
 }
 </script>
 
 <style scoped>
-    /* *{
-        margin: 0px;
-        padding: 0px;
-    } */
-    .not1{
-        width: 600px;
-    }
-    .notiTitle {
-        width: 565px;
-        font-size: 15px;
-        margin: 0px;
-        font-weight: bold;
-        /* clear: both; */
-        color: #259fc8;
-        text-decoration: none;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-    .notiMain {
-        color: #333;
-        font-size: 13px;
-        line-height: 22px;
-        padding: 5px 0;
-        word-wrap: break-word;
-        border-bottom:1px solid rgba(0, 0, 0, 0.1);
-    }
-    .notiSource {
-        margin-top: 5px;
-        color: #999;
-        line-height: 20px;
-
-    }
-     .notiInfo span {
-        color: #259fc8;
-    }
+.notiTitle {
+    width: 565px;
+    font-size: 15px;
+    margin: 0px;
+    font-weight: bold;
+    /* clear: both; */
+    color: #259fc8;
+    text-decoration: none;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.notiMain {
+    color: #333;
+    font-size: 13px;
+    line-height: 22px;
+    padding: 5px 0;
+    word-wrap: break-word;
+    border-bottom:1px solid rgba(0, 0, 0, 0.1);
+}
+.notiSource {
+    margin-top: 5px;
+    color: #999;
+    line-height: 20px;
+}
+.notiInfo span {
+    color: #259fc8;
+}
 </style>
